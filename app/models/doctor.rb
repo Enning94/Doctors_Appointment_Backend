2566d9dd4e925bbc796544048e9b5f4b5ac96a46
Enning94 <enningfestus@gmail.com>
2023-09-28 23:03:14 +0000

    add validations for creating a doctor

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -2,4 +2,12 @@ class Doctor < ApplicationRecord
   belongs_to :location
   has_many :appointments, dependent: :destroy
   has_many :patients, through: :appointments
+
+  
+  validates :name, presence: true, length: { maximum: 200 }
+  validates :profile_pic, presence: true
+  validates :bio, presence: true, length: { maximum: 1000 }
+  validates :specialization, presence: true, length: { maximum: 100 }
+  validates :consultation_fee, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :location, presence: true
 end
