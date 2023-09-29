10076bff841f8287da5b03802e525dcf5dc58ef7
Ankit <ankit26kushwa@gmail.com>
2023-09-29 23:02:54 +0530

    Update schema and seeds

--- db/schema.rb
+++ db/schema.rb
@@ -10,37 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_29_100418) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_29_091753) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "active_storage_attachments", force: :cascade do |t|
-    t.string "name", null: false
-    t.string "record_type", null: false
-    t.bigint "record_id", null: false
-    t.bigint "blob_id", null: false
-    t.datetime "created_at", null: false
-    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
-    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
-  end
-
-  create_table "active_storage_blobs", force: :cascade do |t|
-    t.string "key", null: false
-    t.string "filename", null: false
-    t.string "content_type"
-    t.text "metadata"
-    t.string "service_name", null: false
-    t.bigint "byte_size", null: false
-    t.string "checksum"
-    t.datetime "created_at", null: false
-    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
-  end
-
-  create_table "active_storage_variant_records", force: :cascade do |t|
-    t.bigint "blob_id", null: false
-    t.string "variation_digest", null: false
-    t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
-  end
 
   create_table "appointments", force: :cascade do |t|
     t.bigint "patient_id", null: false
@@ -48,7 +20,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_29_100418) do
     t.datetime "appointment_date", null: false
     t.string "city", null: false
     t.integer "appointment_duration", null: false
-    t.decimal "facility_fee", default: "200.0", null: false
+    t.decimal "facility_fee", default: "200.0"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["doctor_id"], name: "index_appointments_on_doctor_id"
@@ -57,7 +29,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_29_100418) do
 
   create_table "doctors", force: :cascade do |t|
     t.string "name", null: false
-    t.binary "profile_pic"
+    t.binary "profile_pic", null: false
     t.text "bio", null: false
     t.string "specialization", null: false
     t.decimal "consultation_fee", null: false
@@ -74,8 +46,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_29_100418) do
     t.datetime "updated_at", null: false
   end
 
-  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "appointments", "doctors"
   add_foreign_key "appointments", "patients"
 end
