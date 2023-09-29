43fbaf1c3c0f107680c5344b373142388c3f9d97
Enning94 <enningfestus@gmail.com>
2023-09-29 10:18:40 +0000

    update API routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
-  resources :appointments
-  resources :doctors
-  resources :patients
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   namespace :api do
     root "doctors#index"
+    resources :appointments
+    resources :doctors
+    resources :patients
           end
 
   # Defines the root path route ("/")
