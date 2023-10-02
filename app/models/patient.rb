2ab2ae4e5b8a6638028566221290fbbe3da36cf2
Enning94 <enningfestus@gmail.com>
2023-10-02 06:33:50 +0000

    validate patient & doctor model

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,4 +1,9 @@
 class Patient < ApplicationRecord
   has_many :appointments, dependent: :destroy
   has_many :doctors, through: :appointments
+
+
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
 end
