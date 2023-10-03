79b7ec9f8f497fa1ff1b75052fe5ed3592891d9f
Ankit <ankit26kushwa@gmail.com>
2023-10-03 15:52:44 +0530

    Add username to login

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,27 +1,38 @@
-# frozen_string_literal: true
-
 class Users::SessionsController < Devise::SessionsController
-  # skip_before_action :verify_authenticity_token, only: [:signin, :signup, :signout]
   include RackSessionsFix
 
   respond_to :json
-  private
 
-  def respond_with(current_user, _opts = {})
-    render json: {
-      status: { 
-        code: 200, message: 'Logged in successfully.',
-        data: { user: UserSerializer.new(current_user).serializable_hash[:data][:attributes] }
-      }
-    }, status: :ok
+  # Create a new session (login)
+  def create
+    self.resource = find_resource
+
+    if resource&.valid_password?(sign_in_params[:password])
+      sign_in(resource_name, resource)
+      yield resource if block_given?
+
+      render json: {
+        status: {
+          code: 200,
+          message: 'Logged in successfully.',
+          data: { user: UserSerializer.new(resource).serializable_hash[:data][:attributes] }
+        }
+      }, status: :ok
+    else
+      render json: {
+        status: 401,
+        message: 'Invalid email/username or password.'
+      }, status: :unauthorized
+    end
   end
 
   def respond_to_on_destroy
     if request.headers['Authorization'].present?
-      jwt_payload = JWT.decode(request.headers['Authorization'].split(' ').last, ENV['DEVISE_JWT_SECRET_KEY']).first
+      jwt_payload = JWT.decode(request.headers['Authorization'].split.last,
+                               ENV.fetch('DEVISE_JWT_SECRET_KEY', nil)).first
       current_user = User.find(jwt_payload['sub'])
     end
-    
+
     if current_user
       render json: {
         status: 200,
@@ -33,5 +44,30 @@ class Users::SessionsController < Devise::SessionsController
         message: "Couldn't find an active session."
       }, status: :unauthorized
     end
+  end
+
+  private
+
+  # Override find_for_database_authentication to allow login with both email and username
+  def find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+    if (login = conditions.delete(:login)).present?
+      where(conditions.to_h).where(['lower(email) = :value OR lower(username) = :value',
+                                    { value: login.downcase }]).first
+    else
+      where(conditions.to_h).first
+    end
+  end
+
+  # Permit both :email and :password parameters for login
+  def sign_in_params
+    params.require(:user).permit(:password, :login)
+  end
+
+  def find_resource
+    login = sign_in_params[:login]
+    return unless login.present?
+
+    User.find_by(email: login) || User.find_by(username: login)
   end
 end
