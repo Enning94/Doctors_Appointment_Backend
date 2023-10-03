39e9b4c4d61d0b9d33c8374f0c0a47f4e35bc188
Ankit <ankit26kushwa@gmail.com>
2023-10-03 15:03:22 +0530

    update routes and schema

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,20 @@
 Rails.application.routes.draw do
+  get '/current_user', to: 'current_user#index'
+  # devise_for :users
+  devise_for :users, path: '', path_names: {
+    sign_in: 'login',
+    sign_out: 'logout',
+    registration: 'signup'
+  },
+  controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   namespace :api do
     namespace :v1 do
-      root "doctors#index"
+      # root "doctors#index"
       resources :appointments
       resources :doctors
       resources :patients
