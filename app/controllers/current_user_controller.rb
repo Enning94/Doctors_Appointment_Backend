70eefa358790efc55889494d17b1abdb16fef4a1
Ankit <ankit26kushwa@gmail.com>
2023-10-03 15:03:07 +0530

    Create current_user controller

---
+++ app/controllers/current_user_controller.rb
@@ -0,0 +1,12 @@
+class CurrentUserController < ApplicationController
+  #  include RackSessionsFix 
+  #  before_action :authenticate_user!
+
+  def index
+    if current_user.present?
+      render json: { user: UserSerializer.new(current_user).serializable_hash[:data][:attributes], status: :ok }
+    else
+      render json: { error: 'User not authenticated' }, status: :unauthorized
+    end
+  end
+end
